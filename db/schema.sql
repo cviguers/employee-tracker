08250d9edfc796baf102bc3b75401e482cd14a58
cviguers <cviguers@gmail.com>
2023-04-11 17:58:31 -0400

    tables updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,20 @@
-DROP DATABASE IF EXISTS books_db;
-CREATE DATABASE books_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE books_db;
+USE employee_db;
 
-CREATE TABLE favorite_books (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL
+CREATE TABLE department (
+  id INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  salary DECIMAL NOT NULL,
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
 );
