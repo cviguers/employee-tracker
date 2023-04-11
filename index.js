63e1494f97c100f8e12643952002e973b5ee0d46
cviguers <cviguers@gmail.com>
2023-04-11 16:25:27 -0400

    specs inserted into each util function

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 // include packages and files needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const {viewDepartments, viewRoles, viewEmployees, addDepartments, addRole, addEmployee, updateEmployeeRole} = require('./utils/directory');
 
 
-// array of questions for user input
-inquirer
-    .prompt([
-    {
-        type: 'list',
-        name: 'directory',
-        message: 'What would you like to do?',
-        choices: [
-            'view all departments',
-            'view all roles',
-            'view all employees',
-            'add a department',
-            'add a role',
-            'add an employee',
-            'update an employee role'
-        ],
-    }
-    ])
-    .then((data) => {
-        const directoryChoice = data;
-        switch(directory) {
-            case 'view all departments':
-                directoryChoice = viewAllDepartments();
-            break;
-            case 'view all roles':
-                directoryChoice = viewAllRoles();
-            break;
-            case 'view all employees':
-                directoryChoice = viewAllEmployees();
-            break;
-            case 'add a department':
-                directoryChoice = viewAllDepartments();
-            break;
-            case 'add a role':
-                directoryChoice = viewAllDepartments();
-            break;
-            case 'add an employee':
-                directoryChoice = viewAllDepartments();
-            break;
-            case 'update an employee role':
-                directoryChoice = viewAllDepartments();
-            break;
+const init = function() {
+    // array of questions for user input
+    inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'directory',
+            message: 'What would you like to do?',
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update an employee role'
+            ],
         }
-    })
+        ])
+        .then((data) => {
+            // dependent on directory choice, call corresponding function
+            let directoryChoice = data.directory;
+            switch(directory) {
+                case 'view all departments':
+                    directoryChoice = viewDepartments();
+                break;
+                case 'view all roles':
+                    directoryChoice = viewRoles();
+                break;
+                case 'view all employees':
+                    directoryChoice = viewEmployees();
+                break;
+                case 'add a department':
+                    directoryChoice = addDepartments();
+                break;
+                case 'add a role':
+                    directoryChoice = addRole();
+                break;
+                case 'add an employee':
+                    directoryChoice = addEmployee();
+                break;
+                case 'update an employee role':
+                    directoryChoice = updateEmployeeRole();
+                break;
+            }
+        })
+        // log errors, if any
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+// call for app to begin
+init();
---
+++ utils/directory.js
@@ -0,0 +1,29 @@
+function viewDepartments() {
+    // THEN I am presented with a formatted table showing department names and department ids
+};
+
+function viewRoles() {
+    // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+};
+
+function viewEmployees() {
+    // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+};
+
+function addDepartments() {
+    // THEN I am prompted to enter the name of the department and that department is added to the database
+};
+
+function addRole() {
+    // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+};
+
+function addEmployee() {
+    // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+};
+
+function updateEmployeeRole() {
+    // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+};
+
+module.exports = { {viewDepartments, viewRoles, viewEmployees, addDepartments, addRole, addEmployee, updateEmployeeRole} }
