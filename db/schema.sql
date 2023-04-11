b168329f95b82c657803a5416e3329fe84efcd15
cviguers <cviguers@gmail.com>
2023-04-11 18:44:35 -0400

    seeds initialized

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
+  -- department_id: INT NOT NULL,
+  -- FOREIGN KEY
+  -- REFERENCES
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  -- role_id: INT NOT NULL,
+  -- manager_id INT NOT NULL,
+  -- FOREIGN KEY
+  -- REFERENCES
+  -- FOREIGN KEY
+  -- REFERENCES
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,25 @@
-INSERT INTO favorite_books (book_name)
-VALUES ("The Great Gatsby"),
-       ("Huckleberry Finn"),
-       ("100 Years of Solitude"),
-       ("Things Fall Apart"),
-       ("Crime and Punishment"),
-       ("Moby Dick"),
-       ("Decameron");
-       
+INSERT INTO department (department_name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales"),
+
+INSERT INTO role (title, salary)
+VALUES ("Sales Lead, 100000"),
+       ("Salesperson, 80000"),
+       ("Lead Engineer, 150000"),
+       ("Software Engineer, 120000"),
+       ("Account Manager, 160000"),
+       ("Accountant, 125000"),
+       ("Legal Team Lead, 250000"),
+       ("Lawyer, 190000"),
+
+INSERT INTO role (first_name, last_name)
+VALUES ("John, Doe"),
+       ("Mike, Chan"),
+       ("Ashley, Rodrieguez"),
+       ("Kevin, Tupik"),
+       ("Kunal, Singh"),
+       ("Malia, Brown"),
+       ("Sarah, Lourd"),
+       ("Tom, Allen"),
