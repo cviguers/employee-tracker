b3503169e50bf632442395ba748b3851002dfb69
cviguers <cviguers@gmail.com>
2023-04-12 10:01:57 -0400

    final push before class

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,14 @@ USE employee_db;
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES departments(id)
 );
@@ -20,6 +21,7 @@ CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  roles INT,
   FOREIGN KEY (roles_id)
   REFERENCES roles(id)
   FOREIGN KEY (manager_id)
